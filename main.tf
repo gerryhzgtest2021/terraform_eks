fae5ac4ce7a0a399686fede46d979b887c6dc8d7
Gerry Huang <gerryhzgtest2021@yahoo.com>
2021-10-20 06:26:05 -0700

    create eks cluster

---
+++ main.tf
@@ -0,0 +1,54 @@
+locals {
+  aws_region = "us-east-1"
+  env        = "test"
+}
+
+provider "aws" {
+  region = local.aws_region
+}
+
+data "aws_eks_cluster" "eks" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.2.0"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  name            = "main"
+  private_subnets = ["10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.0.0/24", "10.0.1.0/24"]
+  cidr            = "10.0.0.0/16"
+  #vpc
+  enable_dns_hostnames = true
+  enable_classiclink   = false
+  #NAT Gateway
+  enable_nat_gateway = true
+}
+
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_version = "1.21"
+  cluster_name    = "${local.env}-cluster"
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.private_subnets
+
+  worker_groups = [
+    {
+      instance_type = "t2.micro"
+      asg_max_size  = 5
+    }
+  ]
+}
